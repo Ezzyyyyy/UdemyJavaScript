a8b585d2e2e1cbf076888109208058afd52afa4c
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-10-31 11:43:52 +0000

    Added and commented out different ways to use operators

--- operations.js
+++ operations.js
@@ -3,3 +3,18 @@ console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4); // gives remainder from calculation 10 / 4 => 4 * 2 = 8 10 - 2 == 2
+
+console.log((10 + 3 - 5) * 10); // Works like normal maths, Multiplication first. Brackets can help with this.
+
+
+
+
+
+
+
+//let result = (10 + 3 - 5) * 10;
+//result = 10 * 4;
+//result++; //result = result + 1
+//result--; // result = result - 1
+
+//result += 5; // result = result + 5 (Can do this with any operator + - * /)
