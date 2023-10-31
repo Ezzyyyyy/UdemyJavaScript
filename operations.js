e1b1ba5d23169deaf648858bc496ea44b6a459a2
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-10-31 11:37:52 +0000

    Added explanation as to how modulus works

--- operations.js
+++ operations.js
@@ -2,4 +2,4 @@ console.log(10 + 4);
 console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
-console.log(10 % 4);
+console.log(10 % 4); // gives remainder from calculation 10 / 4 => 4 * 2 = 8 10 - 2 == 2
